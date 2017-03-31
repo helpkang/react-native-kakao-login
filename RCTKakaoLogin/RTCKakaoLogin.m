9e5da02380c71440e8480c3daddac8aab46a2ba8
Jeff Gu Kang <sandfox2010@gmail.com>
2017-03-31 17:25:52 +0900

    Make login to async in iOS

--- RCTKakaoLogin/RTCKakaoLogin.m
+++ RCTKakaoLogin/RTCKakaoLogin.m
@@ -18,15 +18,15 @@ RCT_REMAP_METHOD(login,
                  resolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
-
+  dispatch_async(dispatch_get_main_queue(), ^{
     [[KOSession sharedSession] close];
     [[KOSession sharedSession] openWithCompletionHandler:^(NSError *error) {
         //        if(error) {
         //            reject(@"kakao login error", @"not login", error);
         //            return;
         //        }
-        
-        
+
+
         if ([[KOSession sharedSession] isOpen]) {
             [self loginProcessResolve:resolve
                  rejecter:reject];
@@ -37,7 +37,7 @@ RCT_REMAP_METHOD(login,
             reject(@"KAKAO_LOGIN_CANCEL", @"CANCEL", nil);
         }
     }];
-    
+  });
 }
 
 - (void)loginProcessResolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject
@@ -45,16 +45,16 @@ RCT_REMAP_METHOD(login,
     // login success
     NSLog(@"access toeken %@",[KOSession sharedSession].accessToken);
     NSLog(@"login succeeded.");
-    
-    
+
+
     [KOSessionTask  meTaskWithCompletionHandler :^(KOUser* result, NSError *error) {
-        
+
         if (result) {
             // success
             NSLog(@"userId=%@", result.ID);
             NSLog(@"nickName=%@", [result propertyForKey:@"nickname"]);
             NSLog(@"profileImage=%@", [result propertyForKey:@"profile_image"]);
-            
+
             NSDictionary *userSession = @{
                                           @"accessToken": [KOSession sharedSession].accessToken,
                                           @"id": result.ID,
@@ -66,13 +66,13 @@ RCT_REMAP_METHOD(login,
             NSLog(@"successs.................");
             resolve(userSession);
             return;
-            
+
         } else {
             NSLog(@"login failed.");
             //                    NSError *error = [NSError errorWithDomain:@"kakaologin" code:2 userInfo:nil];
             reject(@"KAKAO_LOGIN_FAIL", @"FAIL", nil);
         }
-        
+
     }];
 }
 

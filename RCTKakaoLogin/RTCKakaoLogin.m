2fe845853c47930c296f38163fdd6cbb16d954d5
joo yun-whoi <yunhoi129@gmail.com>
2018-06-04 11:23:28 +0900

    ios 버튼 텍스트 안보이는 부분 수정

--- RCTKakaoLogin/RTCKakaoLogin.m
+++ RCTKakaoLogin/RTCKakaoLogin.m
@@ -18,26 +18,27 @@ RCT_REMAP_METHOD(login,
                  resolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
-
-    [[KOSession sharedSession] close];
-    [[KOSession sharedSession] openWithCompletionHandler:^(NSError *error) {
-        //        if(error) {
-        //            reject(@"kakao login error", @"not login", error);
-        //            return;
-        //        }
-        
-        
-        if ([[KOSession sharedSession] isOpen]) {
-            [self loginProcessResolve:resolve
-                 rejecter:reject];
-        } else {
-            // failed
-            NSLog(@"login cancel.");
-            //NSError *error = [NSError errorWithDomain:@"kakaologin" code:1 userInfo:nil];
-            reject(@"KAKAO_LOGIN_CANCEL", @"CANCEL", nil);
-        }
-    }];
     
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[KOSession sharedSession] close];
+        [[KOSession sharedSession] openWithCompletionHandler:^(NSError *error) {
+            //        if(error) {
+            //            reject(@"kakao login error", @"not login", error);
+            //            return;
+            //        }
+            
+            
+            if ([[KOSession sharedSession] isOpen]) {
+                [self loginProcessResolve:resolve
+                                 rejecter:reject];
+            } else {
+                // failed
+                NSLog(@"login cancel.");
+                //NSError *error = [NSError errorWithDomain:@"kakaologin" code:1 userInfo:nil];
+                reject(@"KAKAO_LOGIN_CANCEL", @"CANCEL", nil);
+            }
+        }];
+    });
 }
 
 - (void)loginProcessResolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject
